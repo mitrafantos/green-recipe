9b14ae97db08b91c25b0053b05250725513ec35f
Dmitriy Troitskiy <mitrafantos@gmail.com>
2019-12-02 00:41:01 +0300

    testing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.redirect('/recipes');
+  // res.redirect('/recipes');
+  res.send('success');
 });
 
 module.exports = router;
