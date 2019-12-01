75b65ec241360cb8d86c99117694437fe0887720
Dmitriy Troitskiy <mitrafantos@gmail.com>
2019-12-02 00:43:34 +0300

    test worked

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  // res.redirect('/recipes');
-  res.send('success');
+  res.redirect('/recipes');
+  // res.send('success');
 });
 
 module.exports = router;
